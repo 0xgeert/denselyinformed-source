8a50e2cd9b3604f437e89a01fa53d560112b0064
Geert-Jan Brits <info@clearskyabove.com>
2013-08-13 18:11:13 +0200

    added tools.js which can be used as extension point to hook into docpad generation flow. Nice

---
+++ tools.js
@@ -0,0 +1,53 @@
+
+// Add DocPad to our Application
+var docpadInstance,
+	_ = require("lodash");
+
+var docpadInstanceConfiguration = {
+    env: 'static'
+};
+
+var balUtil = require('bal-util'),
+	extractOptsAndCallback = require('extract-opts').extractOptsAndCallback;
+
+docpadInstance = require('docpad').createInstance(docpadInstanceConfiguration, function(err){
+    if (err)  return console.log(err.stack);
+
+    var that = docpadInstance;
+
+    var generateRender = function(opts, next) {
+		var docpad, _ref1;
+		_ref1 = extractOptsAndCallback(opts, next), opts = _ref1[0], next = _ref1[1];
+		docpad = that;
+		opts.templateData || (opts.templateData = that.getTemplateData());
+		opts.renderPasses || (opts.renderPasses = that.getConfig().renderPasses);
+		balUtil.flow({
+		  object: docpad,
+		  //ORIG
+		  //action: 'contextualizeFiles renderFiles writeFiles',
+		  action: 'contextualizeFiles',
+		  args: [opts],
+		  next: function(err) {
+		    return next(err);
+		  }
+		});
+		return that;
+	};
+
+    //overwrite
+    docpadInstance.generateRender = generateRender;
+
+    docpadInstance.action('generate', function(err){
+		if(err){
+			throw new err;
+		}
+		_.each(docpadInstance.getCollection('html').models, function(doc){
+
+			console.log("///////////////////////////////////////////");
+			console.log("///////////////////////////////////////////");
+			console.log(_.clone(doc.attributes));
+			console.log("---------------------------------------------");
+			console.log(_.clone(doc.getMeta().attributes));
+		})
+    });
+});
